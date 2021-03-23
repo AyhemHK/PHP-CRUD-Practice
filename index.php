ea300d9f193dae33476a07c867478988005234e3
Daiyaan Ijaz <3193289+cheesea3@users.noreply.github.com>
2021-03-23 14:31:51 -0400

    styling fixes
    not important but just take note of what i did
    
    https://getbootstrap.com/docs/5.0/utilities/flex/#justify-content
    
    https://getbootstrap.com/docs/5.0/utilities/spacing/#notation

--- index.php
+++ index.php
@@ -2,23 +2,27 @@
 <html>
 	<head>
 		<title>PHP CRUD</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">		
 		<script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js"></script>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
 	</head>
 	<body>
-		<div class="row justify-content-center">
+	
+	
+		<div class="d-flex justify-content-center">
 		<form action="" method="POST">
-		<div class="form-group">
+		<div class="form-group mb-3">
 		<label>Name</label>
-		<input type="text" name="name" class="form-control" value="Enter your name">	
+		<input type="text" name="name" class="form-control" value="Enter your name"/>	
 		</div>
-		<div class="form-group">
+		<div class="form-group mb-3">
 		<label>Location</label>
-		<input type="text" name="location" class="form-control" value="Enter your name">
+		<input type="text" name="location" class="form-control" value="Enter your name"/>
 		</div>
 		<div class="form-group">
-		<button type="submit" class="btn btn-primary name="save">Save</button>
+		<button type="submit" class="btn btn-primary" name="save">Save</button>
 		</div>
 		</form>
 		</div>
