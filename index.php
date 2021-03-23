431d216a96a8fe6514c0b2dfee8882eaa5c66bd7
Daiyaan Ijaz <3193289+cheesea3@users.noreply.github.com>
2021-03-23 14:15:21 -0400

    fix bootstrap cdn link
    gang

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 	<head>
 		<title>PHP CRUD</title>
 		<script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js"></link>
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js"></script>
 	</head>
 	<body>
