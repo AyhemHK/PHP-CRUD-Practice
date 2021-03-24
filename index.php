14a47d3a4f037056300815af78950767d9736a04
Ayhem Hanna-Kachl <78269271+AyhemHK@users.noreply.github.com>
2021-03-24 12:16:46 -0400

    hidden input field and session message
    if the update button has been clicked, update the record with new $name and $location using the value from the hidden id input field. Set value of $id to 0 outside of if statement.
    
    Set the SESSION message to updated. Set the message type to redirect you back to index.php

--- index.php
+++ index.php
@@ -65,6 +65,7 @@
 		
 		<div class="d-flex justify-content-center">
 		<form action="backend/process.php" method="POST">
+		<input type-"hidden" name="id" value="<?php echo $id; ?>">
 		<div class="form-group mb-3">
 		<label>Name</label>
 		<input type="text" name="name" class="form-control" 
@@ -81,7 +82,7 @@
 		?> 
 			<button type="submit" class="btn btn-info" name="update">Update</button>
 		<?php else: ?>
-		<	button type="submit" class="btn btn-primary" name="save">Save</button>
+		<button type="submit" class="btn btn-primary" name="save">Save</button>
 		<?php endif; ?>
 		</div>
 		</form>
